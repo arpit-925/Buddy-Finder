46cefea35c8c4b37dea6429ea75e76043738f568
arpit-925 <arpitmishra0017@gmail.com>
2024-11-24 15:47:21 +0530

    Add files via upload

---
+++ mini projects/logjs.js
@@ -0,0 +1,11 @@
+
+function showRegister() {
+    document.getElementById("loginForm").classList.add("hidden");
+    document.getElementById("registerForm").classList.remove("hidden");
+  }
+  
+  function showLogin() {
+    document.getElementById("registerForm").classList.add("hidden");
+    document.getElementById("loginForm").classList.remove("hidden");
+  }
+  
